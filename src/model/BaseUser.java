f6517ff59815ff3dc108f405f7634de3f17a1245
Masato Hommaru <15fi110hm@gmail.com>
2017-06-20 00:42:39 +0900

    モデルの追加

--- src/model/BaseUser.java
+++ src/model/BaseUser.java
@@ -1,5 +1,26 @@
 package model;
 
-public abstract class BaseUser {
+public class BaseUser {
+	public enum UserType{
+		TEACHER,
+		STUDENT,
+		ADMINISTRATOR
+	}
+	protected int id;
+	protected String password;
+	protected String mail;
+	protected UserType type;
 
+	public int getId(){
+		return id;
+	}
+
+	public static boolean login(){
+
+	}
+
+	public static void logout(){
+
+	}
 }
+
---
+++ src/model/TeacherDAO.java
@@ -0,0 +1,62 @@
+package model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TeacherDAO {
+	final private static String dbname = "tutorial";   // データベース名
+	final private static String user = "wspuser";      // tutorialにアクセスできるユーザ
+	final private static String password = "hogehoge"; // wspuserのパスワード
+	final private static String driverClassName = "org.postgresql.Driver";
+	final private static String url = "jdbc:postgresql://localhost/" + dbname;
+
+	public static Teacher getTeacherById(int id) throws SQLException {
+		// memberがDBにあるかどうかを調べる
+		boolean result = false;
+		Connection connection;
+		String sql = "select * from member where name=? and pass=?";
+
+		try {
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			pstmt.setString(1, );
+
+			ResultSet resultSet = pstmt.executeQuery();
+			if (resultSet.next()) result = true;
+
+			resultSet.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static Teacher getTeacherById(int id) throws SQLException {
+		// memberがDBにあるかどうかを調べる
+		boolean result = false;
+		Connection connection;
+		String sql = "select * from member where name=? and pass=?";
+
+		try {
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			pstmt.setString(1, );
+
+			ResultSet resultSet = pstmt.executeQuery();
+			if (resultSet.next()) result = true;
+
+			resultSet.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
