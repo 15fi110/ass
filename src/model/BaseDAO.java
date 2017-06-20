8aff8af617f37ae22b3aec904d74e3fb53dc8560
MasatoHommaru <15fi110hm@gmail.com>
2017-06-20 10:58:16 +0900

    BoardCommentの編集

---
+++ src/model/BaseDAO.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class BaseDAO {
+	protected String driverClassName, url, user, password;
+	protected Connection connection;
+	protected ResultSet resultSet;
+
+	protected Properties props;
+	
+	
+	protected void setup(){
+		pros = getProperties("db.properties");
+		driverClassName = props.getProperty("driverClassName");
+		url = props.getProperty("url")+props.getProperty("dbname");
+		user = props.getProperty("user");
+		password = props.getProperty("password");
+
+	}
+
+	protected static Properties getProperties(String filename) {
+    Properties props = new Properties();
+    try {
+       props.load(new FileInputStream(filename));
+    } catch (IOException e) {
+       System.out.println("Warning: " + filename + " is not found.");
+    }
+    return props;
+ }
+}
--- src/model/BoardComment.java
+++ src/model/BoardComment.java
@@ -1,5 +1,43 @@
 package model;
 
 public class BoardComment {
-
+	private int id;
+	private DateTime date;
+	private int year;
+	private String content;
+	private BaseUser user;
+	private boolean canShowTeacher;
+	
+	public BoardComment(int id, DateTime date, int year, String content, BaseUSer user, boolean canShowTeacher){
+		this.id = id;
+		this.date = date;
+		this.year = year;
+		this.content = content;
+		this.user = user;
+		this.canShowTeacher = canShowTeacher;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	public DateTIme getDate(){
+		return date;
+	}
+	public int getYear(){
+		return year;
+	}
+	public String getContent(){
+		
+	}
+	public BaseUser getUser(){
+		
+	}
+	
+	public void register(String content, BaseUser user){
+		
+	}
+	public static List<BoardComment> getList(int lessonId, boolean canShowTeacher){
+		
+	}
+	
 }
