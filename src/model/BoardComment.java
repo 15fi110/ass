b496a702646be17d92aceeb44b28973dd2989ad5
15fi115 <15fi115@home-pc>
2017-06-23 14:27:40 +0900

    update

--- src/model/BoardComment.java
+++ src/model/BoardComment.java
@@ -11,36 +11,49 @@ public class BoardComment {
 	private BaseUser user;
 	private boolean canShowTeacher;
 
-	public BoardComment(int id, Date date, int year, String content, BaseUser user, boolean canShowTeacher){
-		this.id = id;
-		this.date = date;
-		this.year = year;
-		this.content = content;
-		this.user = user;
-		this.canShowTeacher = canShowTeacher;
-	}
-
-	public int getId(){
-		return id;
-	}
-	public Date getDate(){
-		return date;
-	}
-	public int getYear(){
-		return year;
-	}
-	public String getContent(){
-		return content;
-	}
-	public BaseUser getUser(){
-		return user;
-	}
-
 	public void register(String content, BaseUser user){
 
 	}
 	public static ArrayList<BoardComment> getList(int lessonId, boolean canShowTeacher){
 		return null;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public BaseUser getUser() {
+		return user;
+	}
+	public void setUser(BaseUser user) {
+		this.user = user;
+	}
+	public boolean isCanShowTeacher() {
+		return canShowTeacher;
+	}
+	public void setCanShowTeacher(boolean canShowTeacher) {
+		this.canShowTeacher = canShowTeacher;
+	}
+	
+	
 
 }
