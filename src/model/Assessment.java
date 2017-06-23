e661fb1c05614fe025e4934ca90a9f3a03eaa296
15fi115 <15fi115@home-pc>
2017-06-23 14:32:22 +0900

    update

--- src/model/Assessment.java
+++ src/model/Assessment.java
@@ -1,5 +1,42 @@
 package model;
 
-public class Assessment {
+import java.util.Date;
 
+public class Assessment {
+	private int id;
+	private Date date;
+	private int year;
+	private Student student;
+
+	public void register(){
+		
+	}
+	public Assessment aggregate(){
+		return null;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
 }
