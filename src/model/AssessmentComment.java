7e315636eaa5421076c2b811e14fc3cbca0ce694
15fi115 <15fi115@133.14.40.71>
2017-07-14 15:53:01 +0900

    AssessmentCommentDAO 一応完成

--- src/model/AssessmentComment.java
+++ src/model/AssessmentComment.java
@@ -9,12 +9,49 @@ public class AssessmentComment {
 	private int year;
 	private String content;
 	private Student student;
+	private Lesson lesson;
 	
 	public void register(String content, Student student){
-		
+		hogehoge
 	}
 	public static ArrayList<AssessmentComment> getList(int claasId){
 		return null;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public Lesson getLesson() {
+		return lesson;
+	}
+	public void setLesson(Lesson lesson) {
+		this.lesson = lesson;
 	}
 	
 }
--- src/model/AssessmentCommentDAO.java
+++ src/model/AssessmentCommentDAO.java
@@ -1,5 +1,74 @@
 package model;
 
-public class AssessmentCommentDAO {
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
+import model.BaseUser.UserType;
+
+public class AssessmentCommentDAO extends BaseDAO{
+	public ArrayList<AssessmentComment> findByLessonId(int lessonId){
+		ArrayList<AssessmentComment> assessmentCommentList = new ArrayList<AssessmentComment>();
+		PreparedStatement prepStmt_find;
+    	String strPrepSQL_find = "SELECT * FROM assessmentcomment WHERE lessonid = ? ";
+    	
+    	try
+		{
+			setup();
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, user, password);
+			prepStmt_find = connection.prepareStatement(strPrepSQL_find);
+
+			prepStmt_find.setInt(1, lessonId);
+
+			resultSet = prepStmt_find.executeQuery();
+
+			while (resultSet.next()) {
+				AssessmentComment assessmentComment = new AssessmentComment();
+				assessmentComment.setId(resultSet.getInt("id"));
+				assessmentComment.setDate(resultSet.getDate("date"));
+				assessmentComment.setYear(resultSet.getInt("year"));
+				assessmentComment.setContent(resultSet.getString("content"));
+				Student student = (Student)new UserDAO().getUserById(resultSet.getInt("userid"), UserType.STUDENT);
+				assessmentComment.setStudent(student);
+				Lesson lesson = new LessonDAO().findById(lessonId);
+				assessmentComment.setLesson(lesson);
+				
+				assessmentCommentList.add(assessmentComment);
+				}
+			resultSet.close();
+			prepStmt_find.close();
+			connection.close();
+		}catch(
+		Exception e)
+		{
+			e.printStackTrace();
+		}
+		return assessmentCommentList;
+	}
+	
+	public void create(AssessmentComment assessmentComment){
+    	PreparedStatement prepStmt_create;
+    	String strPrepSQL_create = "INSERT INTO assessmentcomment VALUES (nextval('assessmentcomment_id_seq'), ?, ?, ?, ?, ?)";
+    	try{
+    		setup();
+			Class.forName(driverClassName);
+			connection = DriverManager.getConnection(url, user, password);
+			prepStmt_create = connection.prepareStatement(strPrepSQL_create);
+			prepStmt_create.setDate(1, (Date) assessmentComment.getDate());
+			prepStmt_create.setInt(2, assessmentComment.getYear());
+			prepStmt_create.setString(3, assessmentComment.getContent());
+			prepStmt_create.setInt(4, assessmentComment.getStudent().getId());
+			prepStmt_create.setInt(5, assessmentComment.getLesson().getId());
+
+			prepStmt_create = connection.prepareStatement(strPrepSQL_create);
+			prepStmt_create.executeUpdate();
+			
+			prepStmt_create.close();
+			connection.close();
+    	}catch(Exception e){
+			e.printStackTrace();
+    	}
+    }
 }
